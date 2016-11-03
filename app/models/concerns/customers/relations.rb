a3cb0f59391a3fe2428715ca58676d3ae972a515
Ismael Isuani <ismaelisuani@gmail.com>
2016-11-03 17:56:19 -0300

    Add customers restrictions again

--- app/models/concerns/customers/relations.rb
+++ app/models/concerns/customers/relations.rb
@@ -2,7 +2,7 @@ module Customers::Relations
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :adviser, optional: true
+    belongs_to :adviser
     belongs_to :card
     belongs_to :city
     belongs_to :education_level
@@ -11,7 +11,7 @@ module Customers::Relations
     belongs_to :neighborhood, optional: true
     belongs_to :occupation
     belongs_to :state
-    belongs_to :workgroup, optional: true
+    belongs_to :workgroup
     has_many :references, dependent: :destroy
     has_many :loans, dependent: :destroy
     has_many :member_payments, dependent: :destroy
