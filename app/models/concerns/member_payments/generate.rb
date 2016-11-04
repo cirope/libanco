5d5305c8f4a3a8219fcf5869a13592647371f1c7
Ismael Isuani <ismaelisuani@gmail.com>
2016-11-04 08:08:27 -0300

    Fix member payments

--- app/models/concerns/member_payments/generate.rb
+++ app/models/concerns/member_payments/generate.rb
@@ -3,13 +3,15 @@ module MemberPayments::Generate
 
   module ClassMethods
     def generate
-      mtoday = Date.today.mday
-      mdays = mtoday == Date.today.end_of_month.mday ? Array(mtoday..31) : [mtoday]
+      today = Date.today
+      mdays = today.mday == today.end_of_month.mday ? Array(today.mday..31) : [today.mday]
 
       mdays.each do |mday|
+        expire_at = today.next_month.change(day: mday) rescue today.next_month
+
         Customer.members.where('starting_day = ?', mday).each do |customer|
           customer.member_payments.create!(
-            amount: customer.card.monthly_fee.to_f, expire_at: 1.month.from_now.to_date
+            amount: customer.card.monthly_fee.to_f, expire_at: expire_at
           )
         end
       end
