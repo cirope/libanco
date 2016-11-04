af76acb154e2a88aadf9df3f14d65282e0f7b09d
Ismael Isuani <ismaelisuani@gmail.com>
2016-11-03 23:06:33 -0300

    Fix expire_at

--- app/models/concerns/member_payments/generate.rb
+++ app/models/concerns/member_payments/generate.rb
@@ -3,15 +3,13 @@ module MemberPayments::Generate
 
   module ClassMethods
     def generate
-      mtoday = 30 #Date.today.mday
+      mtoday = Date.today.mday
       mdays = mtoday == Date.today.end_of_month.mday ? Array(mtoday..31) : [mtoday]
 
       mdays.each do |mday|
-        expire_at = (Time.now.beginning_of_month + (mday-1).days) + 1.month
-
         Customer.members.where('starting_day = ?', mday).each do |customer|
           customer.member_payments.create!(
-            amount: customer.card.monthly_fee.to_f, expire_at: expire_at.to_date
+            amount: customer.card.monthly_fee.to_f, expire_at: 1.month.from_now.to_date
           )
         end
       end
