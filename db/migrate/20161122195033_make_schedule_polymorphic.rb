7bf9f3d4edf92a381cc34d262982db9729abd207
Ismael Isuani <ismaelisuani@gmail.com>
2016-12-16 19:36:34 -0300

    Remove migrations

--- db/migrate/20161122195033_make_schedule_polymorphic.rb
+++
@@ -1,6 +0,0 @@
-class MakeSchedulePolymorphic < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :schedules, :customer
-    add_reference :schedules, :schedulable, polymorphic: true, index: true
-  end
-end
