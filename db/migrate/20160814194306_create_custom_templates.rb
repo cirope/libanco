e95fc2a04d72239806d8703e022913b15e0a6674
Ismael Isuani <ismaelisuani@gmail.com>
2016-12-20 10:22:34 -0300

    Fix migrations

--- db/migrate/20160814194306_create_custom_templates.rb
+++ db/migrate/20160814194306_create_custom_templates.rb
@@ -1,7 +1,9 @@
 class CreateCustomTemplates < ActiveRecord::Migration[5.0]
   def change
     create_table :custom_templates do |t|
+      t.string :kind, null: false
       t.string :name, null: false
+      t.text :content, null: false
       t.references :header_template, index: true
       t.references :footer_template, index: true
       t.integer :lock_version, null: false, default: 0
