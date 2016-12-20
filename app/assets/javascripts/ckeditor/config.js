143f93caff0859cace4e665f175f887c89e24f92
Ismael Isuani <ismaelisuani@gmail.com>
2016-12-20 16:22:36 -0300

    Ckeditor language

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,5 +1,4 @@
 CKEDITOR.editorConfig = function (config) {
-  config.language = "es",
   config.toolbar = [
 		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Save', 'DocProps', 'Preview', 'Templates', 'Document' ] },
 		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
